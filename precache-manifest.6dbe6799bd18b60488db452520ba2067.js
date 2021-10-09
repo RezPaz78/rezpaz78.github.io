0d821d35e7f5e789b81fad7d3dacbba8972869dd
reza pazan <rezapazan@rocketmail.com>
2021-10-09 14:04:28 +0330

    init - commit test

---
+++ precache-manifest.6dbe6799bd18b60488db452520ba2067.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358f410aafc653cd9984b1d6a65016ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b53e8f846fd3a3d72364",
+    "url": "/static/css/main.5d25d591.chunk.css"
+  },
+  {
+    "revision": "6bdabe27c6f3f09f10d5",
+    "url": "/static/js/2.6b2296f2.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/static/js/2.6b2296f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53e8f846fd3a3d72364",
+    "url": "/static/js/main.cd0d0985.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "9a77fbaa85fa42b73e3b96399daf49c5",
+    "url": "/static/media/Lato-Thin.9a77fbaa.ttf"
+  },
+  {
+    "revision": "86b9d8725c0f3b22f1db742fcefdfc69",
+    "url": "/static/media/home-bg.86b9d872.jpg"
+  },
+  {
+    "revision": "08e777bae39879194038a6aec69f9cb3",
+    "url": "/static/media/reza.08e777ba.jpg"
+  }
+]);
